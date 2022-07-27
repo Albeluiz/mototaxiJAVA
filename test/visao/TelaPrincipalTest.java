97a88d98c078ab452f1e888a20d170a7d9de76a1
ALBELUIZ SAMPAIO <albeluiz@hotmail.com>
2022-07-27 01:33:16 -0300

    Comite Inicial

---
+++ test/visao/TelaPrincipalTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author albeluiz.santos
+ */
+public class TelaPrincipalTest {
+    
+    public TelaPrincipalTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
